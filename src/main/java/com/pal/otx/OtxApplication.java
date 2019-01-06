4b4863741d22eafd3335bf7054bd786cb83b57c2
Ali <Ali@DESKTOP-QOB5SVD>
2019-01-06 17:12:18 +0200

    Init commit.

---
+++ src/main/java/com/pal/otx/OtxApplication.java
@@ -0,0 +1,14 @@
+package com.pal.otx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtxApplication.class, args);
+	}
+
+}
+
